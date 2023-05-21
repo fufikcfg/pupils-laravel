a77a51929d7965e366ce50a97cc88fabb05708a7
fufikcfg <cool.isaak2016@yandex.ru>
2023-05-21 17:15:03 +0500

    fix bugs for auth and create store

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -2,10 +2,65 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Student\BaseController;
+use App\Http\Requests\StudentsStoreRequest;
+use App\Http\Resources\StudentResource;
+use App\Models\Students;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
-class StudentController extends Controller
+class StudentController extends BaseController
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        Students::all();
+    }
+
+
+    public function store(StudentsStoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $this->service->store($data);
+
+        return $data;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new StudentResource(Students::with('classes')->findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
